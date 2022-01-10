8cbf73c56bfd0ee5f6699f119140fe64e5f7fa19
Rafael Viana <rafaelvinicius@gmail.com>
2022-01-10 15:10:01 -0300

    MovieCard

---
+++ frontend/src/components/MovieScore/index.tsx
@@ -0,0 +1,22 @@
+import { ReactComponent as StarFull } from "assets/img/star-full.svg";
+import { ReactComponent as StarHalf } from "assets/img/star-half.svg";
+import { ReactComponent as StarEmpty } from "assets/img/star-empty.svg";
+import "./styles.css";
+import MovieStars from 'components/MovieStars';
+
+function MovieScore() {
+  const score = 3.5;
+  const count = 13;
+
+  return (
+    <div className="dsmovie-score-container">
+      <p className="dsmovie-score-value">
+        {score > 0 ? score.toFixed(1) : "-"}
+      </p>
+      <MovieStars />
+      <p className="dsmovie-score-count">{count} avaliações</p>
+    </div>
+  );
+}
+
+export default MovieScore;
