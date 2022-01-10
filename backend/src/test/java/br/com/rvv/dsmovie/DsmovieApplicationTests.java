ff5a840e14eeb612363c655db0d34c53ec93015c
Rafael Viana <rafaelvinicius@gmail.com>
2022-01-10 12:09:35 -0300

    Project created.

---
+++ backend/src/test/java/br/com/rvv/dsmovie/DsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rvv.dsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
